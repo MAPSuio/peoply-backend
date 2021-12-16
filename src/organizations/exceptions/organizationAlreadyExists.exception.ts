89928edce4f53bbc4e14d7993b986bb9ef6a3e5c
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2021-12-16 14:51:03 +0100

    changed from HTTP exception to nest exception

--- src/organizations/exceptions/organizationAlreadyExists.exception.ts
+++ src/organizations/exceptions/organizationAlreadyExists.exception.ts
@@ -1,10 +1,7 @@
-import { HttpException, HttpStatus } from "@nestjs/common";
+import { ConflictException } from "@nestjs/common";
 
-export class OrganizationAlreadyExistsException extends HttpException {
+export class OrganizationAlreadyExistsException extends ConflictException {
   constructor(errors: Record<string, string>) {
-    super(
-      { message: "Organization could not be created", errors },
-      HttpStatus.CONFLICT,
-    );
+    super({ message: "Organization could not be created", errors });
   }
 }
