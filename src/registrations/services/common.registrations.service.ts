39d3f9262aa37a3ab45e5e7e2333934c1889171c
Christoffer Bjelke <56649743+chribjel@users.noreply.github.com>
2022-04-29 16:00:26 +0200

    fix delete reg waitlist logic (#159)

--- src/registrations/services/common.registrations.service.ts
+++ src/registrations/services/common.registrations.service.ts
@@ -1,8 +1,12 @@
+import { RegStatus } from ".prisma/client";
 import { Injectable } from "@nestjs/common";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { PrismaError } from "../../prisma/prisma.constants";
 import { PrismaService } from "../../prisma/prisma.service";
-import { RegistrationNotFoundException } from "../exceptions";
+import {
+  ForeignKeyNotFoundException,
+  RegistrationNotFoundException,
+} from "../exceptions";
 
 @Injectable()
 export class CommonRegistrationService {
@@ -18,9 +22,50 @@ export class CommonRegistrationService {
 
   async remove(eventId: string, userId: string) {
     try {
-      return await this.prismaService.registration.delete({
-        where: { eventId_userId: { eventId, userId } },
-      });
+      const registration = await this.prismaService.$transaction(
+        async (trx) => {
+          const event = await trx.event.findUnique({
+            where: { id: eventId },
+            include: { registrations: { orderBy: { updatedAt: "asc" } } },
+          });
+
+          const existingReg = event?.registrations.find(
+            (reg) => reg.userId === userId && reg.eventId === eventId,
+          );
+
+          if (event && existingReg) {
+            const reg = await trx.registration.delete({
+              where: { eventId_userId: { eventId, userId } },
+            });
+
+            if (existingReg.regStatus === RegStatus.GOING) {
+              const waitlisted = event.registrations.filter(
+                (reg) => reg.regStatus === RegStatus.WAITLISTED,
+              );
+
+              if (waitlisted.length !== 0) {
+                const nextGoing = waitlisted[0];
+
+                await trx.registration.update({
+                  where: {
+                    eventId_userId: {
+                      eventId: nextGoing.eventId,
+                      userId: nextGoing.userId,
+                    },
+                  },
+                  data: { regStatus: RegStatus.GOING },
+                });
+              }
+            }
+
+            return reg;
+          } else {
+            throw new ForeignKeyNotFoundException(eventId, userId);
+          }
+        },
+      );
+
+      return registration;
     } catch (error) {
       if (
         error instanceof PrismaClientKnownRequestError &&
